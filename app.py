a8476c81084b4ec4672d3706d28790905fe55ab0
Sujal Kumar <23f3001695@ds.study.iitm.ac.in>
2025-12-01 00:53:07 +0530

    Milestone-HMS Final-Submission

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("Hello World")
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from config import Config 
+from flask import Flask
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+from routes import *
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
